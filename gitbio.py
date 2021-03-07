9d123b5c4075196e41c0df5bc28fb5e02181e27e
Devercises <devercises@gmail.com>
2021-03-07 11:57:21 +0100

    Remove Unused import - stylize and its code

--- gitbio.py
+++ gitbio.py
@@ -1,5 +1,5 @@
 import requests
-from colored import fg, attr, stylize
+from colored import fg, attr
 
 API_URL = 'https://api.github.com/users'
 
@@ -8,8 +8,6 @@ def introAndAsk():
     print("%sWelcome to your GitHub Bio Viewer CLI, that shows your bio in Terminal. If you want to exit, press q and enter on question input to quit program. %s" % (fg(4), attr(0)))
     ask = input("%sWhat's Your Name On GitHub?:%s " % (fg(2), attr(0)))
     print('')
-
-    stylize(ask, fg(1))
 
     if ask == "q":
         exit()
